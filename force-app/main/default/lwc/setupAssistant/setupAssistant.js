bc40960d084c06c7591344f6a8e6005f5815cc2c
Jay Kariesch <kariesch@google.com>
2025-12-18 18:55:45 +0000

    feat(embeddedServiceDeploy): adds embedded service deploy

--- force-app/main/default/lwc/setupAssistant/setupAssistant.js
+++ force-app/main/default/lwc/setupAssistant/setupAssistant.js
@@ -1,25 +1,50 @@
 import { LightningElement, track } from 'lwc';
 import deployMetadata from '@salesforce/apex/SetupAssistantController.deployMetadata';
+import setupEmbeddedServiceDeploy from '@salesforce/apex/EmbeddedServiceSetup.createDeployment';
+import setupMessageChannel from '@salesforce/apex/MessageChannelSetup.install';
+import getSiteInfo from '@salesforce/apex/SiteDiscovery.getSiteInfo';
 
 export default class SetupAssistant extends LightningElement {
     @track isLoading = false;
     @track isSuccess = false;
     @track error;
+    siteName = '';
 
-    handleDeploy() {
+    handleNameChange(event) {
+        this.siteName = event.target.value;
+    }
+
+    get isDeployButtonDisabled() {
+        return this.isLoading || !this.siteName;
+    }
+    
+    async handleDeploy() {
         this.isLoading = true;
         this.error = null;
         this.isSuccess = false;
 
-        deployMetadata()
-            .then(result => {
-                this.isSuccess = true;
-            })
-            .catch(error => {
+        try {
+            
+            await deployMetadata()
+            
+            /**
+             * @type {{
+             *  siteDeveloperName: string;
+             *  siteId: string
+             * }}
+             */
+            const siteInfo = await getSiteInfo({ siteName: this.siteName })
+    
+            await setupMessageChannel()
+            
+            await setupEmbeddedServiceDeploy({ siteDeveloperName: siteInfo.siteDeveloperName })
+
+            this.isSuccess = true;
+
+        } catch(error ){
                 this.error = error.body ? error.body.message : error.message;
-            })
-            .finally(() => {
+        } finally {
                 this.isLoading = false;
-            });
+        }
     }
 }
