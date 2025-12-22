90f702630ee41824dd3fff11c448a4737f2c5be5
Jay Kariesch <kariesch@google.com>
2025-12-22 19:15:38 +0000

    refactor(ExternalClientApp): checks if an ECA exists
    Checks if an ECA exists and displays an alternate UI; adds link to existing ECA with 'enable oauth' instructions.

--- force-app/main/default/lwc/externalClientAppForm/externalClientAppForm.js
+++ force-app/main/default/lwc/externalClientAppForm/externalClientAppForm.js
@@ -3,12 +3,38 @@
  */
 
 // @ts-check
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import createExternalClientApp from '@salesforce/apex/ExternalClientApp.create';
+import getExistingECA from '@salesforce/apex/ExternalClientApp.getExistingECA';
 // @ts-expect-error This is an old deprecated implementation.
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ExternalClientAppForm extends LightningElement {
+    /**
+     * @description Retrieves the ECA's record via Apex DML.
+     */
+    @wire(getExistingECA)
+    wiredResources({ error, data }) {
+        if (error) {
+            console.error('Error checking if external client app exists:', error);  
+        }
+        console.log(data)
+        /**
+         * @type {{
+         *  Id: string;
+         *  ContactEmail: string;
+         * }[] | undefined}
+         */
+        const ecaList = data;
+
+        if (Array.isArray(ecaList) && ecaList.length > 0) {
+            const eca = ecaList[0];
+
+            this.doesExternalAppExist = true;
+            this.contactEmail  = eca.ContactEmail;
+            this.ecaId = eca.Id
+        }
+    }
     /**
      * @type {string}
      * 
@@ -18,9 +44,24 @@ export default class ExternalClientAppForm extends LightningElement {
      */
     contactEmail = '';
 
+    /**
+     * The External Client App Id for the record found via `getExistingECA`.
+     */
+    ecaId = ''
+
+    /**
+     * Indicates whether a record for the ECA's developer name was found in Salesforce. 
+     */
     doesExternalAppExist = false;
 
     isLoading = false;
+
+    /**
+     * @description Opens the GCP AA Client App's page in a new tab. The URL requires a truncated Id, hence `slice`.
+     */
+    handleNavigateToEcaSettings() {
+        window.open(`/lightning/setup/ManageExternalClientApplication/${this.ecaId.slice(0, -3)}/detail/`, '_blank');
+    }
 
     /**
      * @description Updates the contactEmail on change.
